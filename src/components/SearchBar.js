cc621e1cf972c2b8d3bde31b8cb7230847a452bd
Tin Tran <tin.tran0519@gmail.com>
2017-01-25 22:41:45 -0800

    auto-complete drop down complete

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,7 +8,8 @@ class SearchBar extends React.Component {
   constructor(props){
     super(props);
     this.state ={
-      location: ''
+      location: '',
+      list:[]
     }
   }
 
@@ -25,14 +26,18 @@ class SearchBar extends React.Component {
   //     });
   // }
 
+  renderLocations() {
+    // console.log(this.state.list)
+    return this.state.list.map((location,index) =>
+      <option value={location.name} key={index} />
+      )
+  };
+
   changeLocation(e) {
+    let _this = this;
     this.setState({
       [e.target.name]: e.target.value
     })
-  }
-
-  submitSearch(e) {
-    e.preventDefault();
     fetch(`/weather?location=${this.state.location}`)
       .then(function(response){
         if(response.status >= 400) {
@@ -41,8 +46,24 @@ class SearchBar extends React.Component {
         return response.json();
       })
       .then(function(results) {
-        console.log('API call',results);
+        _this.setState({ list: results.RESULTS })
+        console.log('API call',results.RESULTS);
       });
+  }
+
+  submitSearch(e) {
+    e.preventDefault();
+    console.log('submit',this.state.location);
+    // fetch(`/weather?location=${this.state.location}`)
+    //   .then(function(response){
+    //     if(response.status >= 400) {
+    //       throw new Error("Bad response from server");
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(function(results) {
+    //     console.log('API call',results);
+    //   });
   }
 
   render() {
@@ -53,6 +74,7 @@ class SearchBar extends React.Component {
         <div className="form-group">
           <label className="control-label"></label>
           <input
+            list="location_list"
             onChange={this.changeLocation.bind(this)}
             value={this.state.location}
             placeholder="Enter City or ZIP code"
@@ -60,6 +82,9 @@ class SearchBar extends React.Component {
             name="location"
             className="form-control"
            />
+           <datalist id="location_list">
+            {this.renderLocations()}
+           </datalist>
         </div>
       </form>
       );
