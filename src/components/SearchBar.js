f23e41732a0ff382be13f6985aeba501eb16b582
Tin Tran <tin.tran0519@gmail.com>
2017-01-26 23:34:37 -0800

    reformat: removed drop-down auto

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,35 +9,27 @@ class SearchBar extends React.Component {
     super(props);
     this.state ={
       location: '',
-      list:[]
+      list: []
     }
   }
-
-  // componentDidMount(){
-  //   fetch(`/weather?location=${this.state.location}`)
-  //     .then(function(response){
-  //       if(response.status >= 400) {
-  //         throw new Error("Bad response from server");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then(function(results) {
-  //       console.log('API call',results);
-  //     });
-  // }
 
   renderLocations() {
     // console.log(this.state.list)
     return this.state.list.map((location,index) =>
-      <option value={location.name} key={index} data-link={location.l} ref="selectedLocation" />
+      <li key={index}>{location.name}</li>
       )
   };
 
   changeLocation(e) {
-    let _this = this;
     this.setState({
       [e.target.name]: e.target.value
     })
+
+  }
+
+  submitSearch(e) {
+    e.preventDefault();
+    let _this = this;
     fetch(`/location?q=${this.state.location}`)
       .then(function(response){
         if(response.status >= 400) {
@@ -49,51 +41,50 @@ class SearchBar extends React.Component {
         _this.setState({ list: results.RESULTS })
         console.log('API call',results.RESULTS);
       });
-  }
+    // let submitLocation = this.state.location;
+    // let link = this.refs.selectedLocation.dataset.link;
+    // let cities = submitLocation.substr(0, submitLocation.indexOf(','));
+    // let states = submitLocation.substr(submitLocation.indexOf(',') + 2);
 
-  submitSearch(e) {
-    e.preventDefault();
-    let submitLocation = this.state.location
-    let link = this.refs.selectedLocation.dataset.link;
-    let cities = submitLocation.substr(0, submitLocation.indexOf(','));
-    let states = submitLocation.substr(submitLocation.indexOf(',') + 2);
-
-    console.log('city and state', cities, states);
+    // console.log('city and state', cities, states);
     // console.log('submit',this.state.location);
     // console.log('ref',this.refs.selectedLocation.dataset.link);
-    fetch(`/weather/${states}/${cities}?link=${link}`)
-      .then(function(response){
-        if(response.status >= 400) {
-          throw new Error("Bad response from server");
-        }
-        return response.json();
-      })
-      .then(function(results) {
-        console.log('API call',results);
-      });
+    // fetch(`/weather/${states}/${cities}?link=${link}`)
+    //   .then(function(response){
+    //     if(response.status >= 400) {
+    //       throw new Error("Bad response from server");
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(function(results) {
+    //     console.log('API call',results);
+    //   });
   }
 
   render() {
     return (
-      <form onSubmit={this.submitSearch.bind(this)}>
-        <h3>Enter location</h3>
+      <div>
+        <form onSubmit={this.submitSearch.bind(this)}>
+          <h3>Enter location</h3>
 
-        <div className="form-group">
-          <label className="control-label"></label>
-          <input
-            list="location_list"
-            onChange={this.changeLocation.bind(this)}
-            value={this.state.location}
-            placeholder="Enter City or ZIP code"
-            type="text"
-            name="location"
-            className="form-control"
-           />
-           <datalist id="location_list">
-            {this.renderLocations()}
-           </datalist>
-        </div>
-      </form>
+          <div className="form-group">
+            <label className="control-label"></label>
+            <input
+              list="location_list"
+              onChange={this.changeLocation.bind(this)}
+              value={this.state.location}
+              placeholder="Enter City or ZIP code"
+              type="text"
+              name="location"
+              className="form-control"
+             />
+          </div>
+        </form>
+
+        <ul>
+          {this.renderLocations()}
+        </ul>
+      </div>
       );
   }
 }
