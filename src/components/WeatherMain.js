db037c03bc95ad616f520d7294209354045d55ee
Tin Tran <tin.tran0519@gmail.com>
2017-01-30 17:49:10 -0800

    forecast WIP

--- src/components/WeatherMain.js
+++ src/components/WeatherMain.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{components} from 'react';
 import '../assets/WeatherMain.css';
+import { Link } from 'react-router';
 
 class WeatherMain extends React.Component{
   constructor(props){
@@ -12,9 +13,11 @@ class WeatherMain extends React.Component{
       currentTemp:'',
       currentTempIcon:'',
       currentTempDescr: '',
+      forecast: ''
     }
   }
 
+  //load weather API
   componentWillMount() {
     let _this = this;
     fetch(`/weather/${this.props.location.state.selectedLocation.zmw}`)
@@ -33,18 +36,30 @@ class WeatherMain extends React.Component{
           date: results.current_observation.observation_time.slice(16),
           currentTemp: results.current_observation.temp_f,
           currentTempIcon: results.current_observation.icon_url,
-          currentTempDescr: results.current_observation.weather
+          currentTempDescr: results.current_observation.weather,
+          forecast: results.forecast.simpleforecast.forecastday
         })
-        console.log(this.state);
+        console.log(this.state.forecast);
         console.log(this.props.location.state.selectedLocation.zmw)
         })
   }
+
+  renderForecast() {
+    return this.state.forecast.map((day,index) =>
+      <div key={index}>{day.date.weekday}</div>
+      )
+  };
 
   render() {
     return(
       <div className="mainWrapper">
         <div className="row header-top">
-          <h1>{this.state.name}</h1>
+          <h1 id="locationName">{this.state.name}</h1>
+          <div id="locationChange">
+            <button>
+              <Link to="/" className="locationPicker">Change location</Link>
+            </button>
+        </div>
           <h3>{this.state.country}</h3>
           <h4>{this.state.date}</h4>
         </div>
@@ -52,8 +67,22 @@ class WeatherMain extends React.Component{
         <div className="row header-bot">
           <img src={this.state.currentTempIcon} />
           <h3>{this.state.currentTempDescr}</h3>
-          <h1>{this.state.currentTemp}</h1>
+          <h1>{this.state.currentTemp}&deg;</h1>
         </div>
+
+      <div className="row col-sm-8 forecast">
+        <section>
+          <h3>Forecast</h3>
+          {this.renderForecast()}
+        </section>
+      </div>
+
+      <div className="row col-md-4">
+        <section>
+          <h3>Details</h3>
+        </section>
+      </div>
+
       </div>
       )
   }
