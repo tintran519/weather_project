cb20afa22d59f67ba4051a8194b7f36c5043f42c
Tin Tran <tin.tran0519@gmail.com>
2017-02-04 13:52:36 -0800

    metric Toggle complete

--- src/components/WeatherMain.js
+++ src/components/WeatherMain.js
@@ -3,6 +3,7 @@ import '../assets/WeatherMain.css';
 import { Link } from 'react-router';
 import Forecast from './weather/Forecast';
 import Details from './weather/Details';
+import classnames from 'classnames';
 
 class WeatherMain extends React.Component{
   constructor(props){
@@ -12,12 +13,15 @@ class WeatherMain extends React.Component{
       name:'',
       country:'',
       date: '',
-      currentTemp:'',
+      currentTempF:'',
+      currentTempC: '',
       currentTempIcon:'',
       currentTempDescr: '',
       forecast: [],
-      currentHigh: '',
-      currentLow: '',
+      currentHighF: '',
+      currentLowF: '',
+      currentHighC: '',
+      currentLowC: '',
       currentObservation: [],
       detailForecastCurrentDay: [],
       detailForecastCurrentNight: [],
@@ -38,21 +42,33 @@ class WeatherMain extends React.Component{
       .then((results) => {
         console.log(results)
         this.setState({
+          celsius: false,
           name: results.current_observation.display_location.full,
           country: results.current_observation.display_location.country,
           date: results.current_observation.observation_time.slice(16),
-          currentTemp: results.current_observation.temp_f,
+          currentTempF: results.current_observation.temp_f,
+          currentTempC: results.current_observation.temp_c,
           currentTempIcon: results.current_observation.icon_url,
           currentTempDescr: results.current_observation.weather,
           forecast: results.forecast,
-          currentHigh: results.forecast.simpleforecast.forecastday[0].high.fahrenheit,
-          currentLow: results.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+          currentHighF: results.forecast.simpleforecast.forecastday[0].high.fahrenheit,
+          currentLowF: results.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+          currentHighC: results.forecast.simpleforecast.forecastday[0].high.celsius,
+          currentLowC: results.forecast.simpleforecast.forecastday[0].low.celsius,
           currentObservation: results.current_observation,
           detailForecastCurrentDay: results.forecast.txt_forecast.forecastday[0],
           detailForecastCurrentNight: results.forecast.txt_forecast.forecastday[1],
         })
       })
     }
+
+  onCelsiusClick() {
+    this.setState({ celsius: true })
+  }
+
+  onFahrenheitClick() {
+    this.setState({ celsius: false })
+  }
 
   render() {
     return(
@@ -72,14 +88,18 @@ class WeatherMain extends React.Component{
           <img src={this.state.currentTempIcon} />
           <span id="tempDescr">{this.state.currentTempDescr}</span>
           <div>
-            <span id="currentHigh">&uarr;&nbsp;{this.state.currentHigh}&deg;</span>
-            <span id="currentLow">&darr;&nbsp;{this.state.currentLow}&deg;</span>
+            <span id="currentHigh">&uarr;&nbsp;{!this.state.celsius ? this.state.currentHighF : this.state.currentHighC}&deg;</span>
+            <span id="currentLow">&darr;&nbsp;{!this.state.celsius ? this.state.currentLowF : this.state.currentLowC}&deg;</span>
           </div>
-          <h1 id="currentTemp">{this.state.currentTemp}&deg;</h1>
+          <h1 id="currentTemp">{!this.state.celsius ? this.state.currentTempF : this.state.currentTempC}&deg;</h1>
+          <div id="metricControl">
+            <div className={classnames("metricToggle", {"metricToggleOff": !this.state.celsius})} onClick={this.onCelsiusClick.bind(this)}>C</div>
+            <div className={classnames("metricToggle", {"metricToggleOff": this.state.celsius})} onClick={this.onFahrenheitClick.bind(this)}>F</div>
+          </div>
         </div>
 
         <div className="row">
-          {this.state.forecast.length === 0 ? <div>Loading...</div> : <Forecast forecastInfo={this.state.forecast} />}
+          {this.state.forecast.length === 0 ? <div>Loading...</div> : <Forecast forecastInfo={this.state.forecast} celsiusToggle={this.state.celsius} />}
           {this.state.currentObservation.length === 0 ? <div>Loading...</div> : <Details currentObservationInfo={this.state.currentObservation} currentDayDetail={this.state.detailForecastCurrentDay} currentNightDetail={this.state.detailForecastCurrentNight} />}
         </div>
 
