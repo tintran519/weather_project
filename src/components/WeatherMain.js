58464995f53e3bbbe87a38bb3d55663c1f84495a
Tin Tran <tin.tran0519@gmail.com>
2017-02-10 23:56:31 -0800

    search bar page styling: globe

--- src/components/WeatherMain.js
+++ src/components/WeatherMain.js
@@ -43,7 +43,6 @@ class WeatherMain extends React.Component{
         return response.json();
       })
       .then((results) => {
-        console.log(results)
         this.setState({
           celsius: false,
           name: results.current_observation.display_location.full,
